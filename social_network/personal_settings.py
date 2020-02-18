a26080f9da497a40094033f070f3e1712d654ede
DukhDmytro <cowboybebop4991@gmail.com>
2020-02-18 14:12:08 +0200

    initial commit

---
+++ social_network/personal_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY_ = 'Your secret key'
+
+DATABASES_ = {
+    'default': {
+        'ENGINE': 'django.db.backends.YourDBMS',
+        'NAME': 'db name',
+        'USER': 'user name',
+        'PASSWORD': 'user password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
